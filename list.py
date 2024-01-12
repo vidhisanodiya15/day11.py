f4d2f183587d6a1a94c96f7193152172c73f35fa
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-12 22:46:58 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,25 @@
+# thislist=["cheery","apple","banana"]
+# print(thislist)
+
+# thislist=["cheery","apple","banana"]
+# print(thislist[1])
+
+
+#thislist=["cherry","banana","apple","orange","grapes","melon"]
+#print(thislist[2:6])
+
+# thislist=["cherry","banana","apple","orange","grapes","melon"]
+# print(thislist[-1])
+
+# thislist=["cherry","banana","orange"]
+# thislist[-1]="blackcurrent"
+# print(thislist)
+
+
+# thislist=["cherry","banana","orange"]
+# thislist.append("blackcurrent")
+# print(thislist)
+
+thislist=["cherry","banana","orange"]
+for x in thislist:
+    print(x)
